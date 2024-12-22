53e8317bb646ed507dc28d221c78afaaf8425b57
Zuhaib Rashid <xuvaxuva006@gmail.com>
2024-12-23 00:52:08 +0530

    add preloader functionality and styles to enhance loading experience and addded favicon also

--- main.js
+++ main.js
@@ -103,3 +103,7 @@ function toggleAnswer(id) {
     icon.innerText = '+';
   }
 }
+var loader = document.getElementById("preloader")
+window.addEventListener("load", function(){
+  loader.style.display = "none"
+})
